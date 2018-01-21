895e4e3942f7b1f2ad5236c3db9a6fa57adb5667
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-21 11:03:38 +0700

    Configure eslint.rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,40 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb",
+  "env": {
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module"
+  },
+  "rules": {
+    "class-methods-use-this": [0
+    ],
+    "consistent-return": 0,
+    "max-len": [2,
+      {
+        "code": 120
+      }
+    ],
+    "no-console": 0,
+    "no-use-before-define": [2,
+      {
+        "functions": true,
+        "classes": false
+      }
+    ],
+    "no-unused-vars": [2,
+      {
+        "args": "all",
+        "caughtErrors": "none"
+      }
+    ],
+    "object-property-newline": [2,
+      {
+        "allowAllPropertiesOnSameLine": true
+      }
+    ],
+    "prefer-destructuring": [0
+    ]
+  }
 };
