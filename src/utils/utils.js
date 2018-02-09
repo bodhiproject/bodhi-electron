2d97f2b17739ae35b40fa625cf394fe75a9ac4e1
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-09 17:02:25 +0700

    Fix path for dev nedb and logs

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,10 +5,14 @@ class Utils {
   static getRootPath() {
     if (_.indexOf(process.argv, '--dev') === -1) {
       // ran without --dev flag (build version)
+      // execPath = dir of program execution
       return path.dirname(process.execPath);
     } else {
       // ran with --dev flag (dev version)
-      return path.dirname(__dirname);
+      // start in src dir
+      const srcDir = path.dirname(__dirname);
+      // go up one to root
+      return path.resolve(srcDir + '/../');
     }
   }
 }
