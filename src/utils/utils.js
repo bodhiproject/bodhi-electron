907740b9085c314bbe323a6798cfa724108124d7
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-09 16:45:52 +0700

    Add utils method to get root path and apply to logger

---
+++ src/utils/utils.js
@@ -0,0 +1,13 @@
+class Utils {
+  static getRootPath() {
+    if (_.indexOf(process.argv, '--dev') === -1) {
+      // ran without --dev flag (build version)
+      return path.dirname(process.execPath)};
+    } else {
+      // ran with --dev flag (dev version)
+      return path.dirname(__dirname);
+    }
+  }
+}
+
+module.exports = Utils;
