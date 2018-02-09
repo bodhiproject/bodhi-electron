c58d8b0236d63c7e73f466973a6469b37b0dab38
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-09 16:48:23 +0700

    Fix syntax error

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ class Utils {
   static getRootPath() {
     if (_.indexOf(process.argv, '--dev') === -1) {
       // ran without --dev flag (build version)
-      return path.dirname(process.execPath)};
+      return path.dirname(process.execPath);
     } else {
       // ran with --dev flag (dev version)
       return path.dirname(__dirname);
