d01f1eae8b212c7ab6d6e43a201dcee0342c8a34
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-19 09:41:01 +0700

    FIx path for data dir

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,17 +4,18 @@ const path = require('path');
 const { app } = require('electron');
 const { version } = require('../../package.json');
 
-const DIR_DEV = '/dev';
+const DIR_DEV = 'dev';
 
 class Utils {
   /*
   * Returns the path where the data directory is, and also creates the directory if it doesn't exist.
+  * 
   */
   static getDataDir() {
     let dataDir;
     if (_.indexOf(process.argv, '--dev') === -1) {
       // production
-      dataDir = `${app.getPath('userData')}${version}`;
+      dataDir = `${app.getPath('userData')}/${version}`;
     } else {
       // development
       dataDir = `${app.getPath('userData')}/${DIR_DEV}`;
