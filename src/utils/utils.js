72dfde9827f290038b7276ce73874bf5fea55d55
Bowen Zhang <bowen@bohdi.network>
2018-02-22 11:16:34 -0800

    remove useless line

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,13 +9,6 @@ const DIR_DEV = 'dev';
 class Utils {
   /*
   * Returns the path where the data directory is, and also creates the directory if it doesn't exist.
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  *
->>>>>>> remove useless subscription and ensure chainBlockNum not null
-=======
->>>>>>> rename batchsynced to onsyncinfo and return SyncInfo Object
   */
   static getDataDir() {
     let dataDir;
