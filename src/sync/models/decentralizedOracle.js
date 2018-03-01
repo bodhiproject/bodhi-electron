d6cebeb06ba0ca03b1dca0210b579f8cc953c120
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-01 12:28:19 +0700

    Convert hex strings for all amounts to number string

--- src/sync/models/decentralizedOracle.js
+++ src/sync/models/decentralizedOracle.js
@@ -1,6 +1,7 @@
 /* eslint no-underscore-dangle: 0 */
 
 const _ = require('lodash');
+const Web3Utils = require('web3-utils');
 
 class DecentralizedOracle {
   constructor(blockNum, txid, rawLog) {
@@ -19,7 +20,7 @@ class DecentralizedOracle {
     this.numOfResults = this.rawLog._numOfResults.toNumber();
     this.lastResultIndex = this.rawLog._lastResultIndex.toNumber();
     this.arbitrationEndTime = this.rawLog._arbitrationEndTime.toNumber();
-    this.consensusThreshold = this.rawLog._consensusThreshold.toJSON();
+    this.consensusThreshold = Web3Utils.hexToNumberString(this.rawLog._consensusThreshold);
   }
 
   translate() {
