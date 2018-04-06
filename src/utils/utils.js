6dacc394ee75f489f58c5954f9b1f79907904b18
Deric Walintukan <deric.walintukan@gmail.com>
2018-04-06 10:14:56 +0900

    Get regex groups to construct data dir

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -19,13 +19,14 @@ class Utils {
     if (_.indexOf(process.argv, '--dev') === -1) {
       const pathPrefix = Config.TESTNET ? 'testnet' : 'mainnet';
 
-      const regexMatches = version.exec(/(\d+)\.(\d+)\.(\d+)-(c\d+)-(d\d+)/g);
-      if (regexMatches === null) {
+      const regex = RegExp(/(\d+)\.(\d+)\.(\d+)-(c\d+)-(d\d+)/g);
+      const regexGroups = regex.exec(version);
+      if (regexGroups === null) {
         throw new Error(`Invalid version number: ${version}`);
       }
       // Example: 0.6.5-c0-d1
       // c0 = contract version 0, d1 = db version 1
-      const versionDir = `${regexMatches[3]}_${regexMatches[4]}` // c0_d1
+      const versionDir = `${regexMatches[4]}_${regexMatches[5]}` // c0_d1
 
       // production
       dataDir = `${osDataDir}/${pathPrefix}/${versionDir}`;
