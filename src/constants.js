d191dac351e118cf62df66e6afd412dde1d06cc2
Deric Walintukan <deric.walintukan@gmail.com>
2018-04-18 11:42:15 -0700

    Add constants for exec files

--- src/constants.js
+++ src/constants.js
@@ -16,6 +16,11 @@ module.exports = {
     FAIL: 'FAIL',
   },
 
+  execFile: {
+    QTUMD: 'qtumd',
+    QTUM_QT: 'qtum-qt',
+  },
+
   BLOCK_0_TIMESTAMP: 1504695029,
   SATOSHI_CONVERSION: 10 ** 8,
 };
