16896f217daf44335a040f39f0d2ddea34050f79
Deric Walintukan <deric.walintukan@gmail.com>
2018-04-27 09:56:53 -0700

    Fix adding transport to prod only

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fs = require('fs-extra');
 const moment = require('moment');
 const winston = require('winston');
-require('winston-papertrail').Papertrail;
+const Papertrail = require('winston-papertrail').Papertrail;
 
 const Utils = require('./utils');
 const { Config } = require('../config/config');
@@ -14,39 +14,42 @@ console.log(`Logs output: ${LOG_DIR}`);
 fs.ensureDirSync(LOG_DIR);
 
 const winstonCfg = winston.config;
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      timestamp() {
-        return moment().format('YYYY-MM-DD HH:mm:ss');
-      },
-      formatter(options) {
-        return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
-      },
-    }),
-    new (winston.transports.File)({
-      filename: `${LOG_DIR}/bodhiapp_${moment().format('YYYYMMDD_HHmmss')}.log`,
-      timestamp() {
-        return moment().format('YYYY-MM-DD HH:mm:ss');
-      },
-      formatter(options) {
-        return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
-      },
-      json: false,
-    }),
-  ],
-  exitOnError: false,
-});
+const transports = [
+  new (winston.transports.Console)({
+    timestamp() {
+      return moment().format('YYYY-MM-DD HH:mm:ss');
+    },
+    formatter(options) {
+      return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
+    },
+  }),
+  new (winston.transports.File)({
+    filename: `${LOG_DIR}/bodhiapp_${moment().format('YYYYMMDD_HHmmss')}.log`,
+    timestamp() {
+      return moment().format('YYYY-MM-DD HH:mm:ss');
+    },
+    formatter(options) {
+      return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
+    },
+    json: false,
+  }),
+];
 
 // add Papertrail remote logging if prod env
 if (!Utils.isDevEnv()) {
-  logger.add(new winston.transports.Papertrail({
+  transports.push(new Papertrail({
     host: 'logs5.papertrailapp.com',
     port: 46145,
     level: 'debug',
+    colorize: true,
     logFormat: (level, message) => `<<< ${level} >>> ${message}`,
   }));
 }
+
+const logger = new (winston.Logger)({
+  transports,
+  exitOnError: false,
+});
 
 const loglvl = process.env.loglvl || Config.DEFAULT_LOGLVL;
 logger.level = loglvl;
