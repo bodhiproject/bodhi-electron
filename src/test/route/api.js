b5479d29a404a3b7f6ac9c6cb140d03cd4c112a9
Bowen Zhang <bowen@bohdi.network>
2018-05-02 14:14:57 -0700

    change the statement for tests

--- src/test/route/api.js
+++ src/test/route/api.js
@@ -8,245 +8,231 @@ const expect = chai.expect;
 let server;
 let routes;
 
-describe('API TESTs', () => {
-  before(function(){
-    function startRestifyServer(){
-      server = restify.createServer({
-        title: 'Bodhi Server',
-      });
-      const cors = corsMiddleware({
-        origins: ['*'],
-      });
-      server.pre(cors.preflight);
-      server.use(cors.actual);
-      server.use(restify.plugins.bodyParser({ mapParams: true }));
-      server.use(restify.plugins.queryParser());
-      server.on('after', (req, res, route, err) => {
-        if (route) {
-          console.debug(`${route.methods[0]} ${route.spec.path} ${res.statusCode}`);
-        } else {
-          console.error(`${err.message}`);
-        }
-      });
-    }
-    startRestifyServer();
+describe('API Routes', () => {
+  before(() => {
+    server = require('../../index').server;
     apiRouter.applyRoutes(server);
     routes = server.routes;
   });
 
-  it('post: is-connected', () => {
-    expect(routes).to.have.property('postisconnected');
+  describe('POST', () => {
+    it('should have the is-connected route', () => {
+      expect(routes).to.have.property('postisconnected');
+    });
+
+    it('should have the get-account-address route', () => {
+      expect(routes).to.have.property('postgetaccountaddress');
+    });
+
+    it('should have the get-transaction route', () => {
+      expect(routes).to.have.property('postgettransaction');
+    });
+
+
+    it('should have the wallet-passphrase route', () => {
+      expect(routes).to.have.property('postwalletpassphrase');
+    });
+
+    it('should have the wallet-lock route', () => {
+      expect(routes).to.have.property('postwalletlock');
+    });
+
+    it('should have the encrypt-wallet route', () => {
+      expect(routes).to.have.property('postencryptwallet');
+    });
+
+    it('should have the wallet-passphrase-change route', () => {
+      expect(routes).to.have.property('postwalletpassphrasechange');
+    });
+
+    it('should have the backup-wallet route', () => {
+      expect(routes).to.have.property('postbackupwallet');
+    });
+
+    it('should have the import-wallet route', () => {
+      expect(routes).to.have.property('postimportwallet');
+    });
+
+    it('should have the get-block route', () => {
+      expect(routes).to.have.property('postgetblock');
+    });
+
+    it('should have the get-block-hash route', () => {
+      expect(routes).to.have.property('postgetblockhash');
+    });
+
+    it('should have the get-transaction-receipt route', () => {
+      expect(routes).to.have.property('postgettransactionreceipt');
+    });
+
+    it('should have the search-logs route', () => {
+      expect(routes).to.have.property('postsearchlogs');
+    });
+
+    it('should have the event-escrow-amount route', () => {
+      expect(routes).to.have.property('posteventescrowamount');
+    });
+
+    it('should have the last-event-factory-index route', () => {
+      expect(routes).to.have.property('postlasteventfactoryindex');
+    });
+
+    it('should have the last-oracle-factory-index route', () => {
+      expect(routes).to.have.property('postlastoraclefactoryindex');
+    });
+
+    it('should have the approve route', () => {
+      expect(routes).to.have.property('postapprove');
+    });
+
+    it('should have the allowance route', () => {
+      expect(routes).to.have.property('postallowance');
+    });
+
+    it('should have the bot-balance route', () => {
+      expect(routes).to.have.property('postbotbalance');
+    });
+
+    it('should have the version route', () => {
+      expect(routes).to.have.property('postversion');
+    });
+
+    it('should have the get-result route', () => {
+      expect(routes).to.have.property('postgetresult');
+    });
+
+    it('should have the bet-balances route', () => {
+      expect(routes).to.have.property('postbetbalances');
+    });
+
+    it('should have the vote-balances route', () => {
+      expect(routes).to.have.property('postvotebalances');
+    });
+
+    it('should have the total-bets route', () => {
+      expect(routes).to.have.property('posttotalbets');
+    });
+
+    it('should have the total-votes route', () => {
+      expect(routes).to.have.property('posttotalvotes');
+    });
+
+    it('should have the create-topic route', () => {
+      expect(routes).to.have.property('postcreatetopic');
+    });
+
+    it('should have the event-factory-version route', () => {
+      expect(routes).to.have.property('posteventfactoryversion');
+    });
+
+    it('should have the withdraw route', () => {
+      expect(routes).to.have.property('postwithdraw');
+    });
+
+    it('should have the withdraw-escrow route', () => {
+      expect(routes).to.have.property('postwithdrawescrow');
+    });
+
+    it('should have the total-qtum-value route', () => {
+      expect(routes).to.have.property('posttotalqtumvalue');
+    });
+
+    it('should have the totalbotvalue route', () => {
+      expect(routes).to.have.property('posttotalbotvalue');
+    });
+
+    it('should have the final-result route', () => {
+      expect(routes).to.have.property('postfinalresult');
+    });
+
+    it('should have the status route', () => {
+      expect(routes).to.have.property('poststatus');
+    });
+
+    it('should have the did-withdraw route', () => {
+      expect(routes).to.have.property('postdidwithdraw');
+    });
+
+    it('should have the winnings route', () => {
+      expect(routes).to.have.property('postwinnings');
+    });
+
+    it('should have the event-address route', () => {
+      expect(routes).to.have.property('posteventaddress');
+    });
+
+    it('should have the consensus-threshold route', () => {
+      expect(routes).to.have.property('postconsensusthreshold');
+    });
+
+    it('should have the finished route', () => {
+      expect(routes).to.have.property('postfinished');
+    });
+
+    it('should have the bet route', () => {
+      expect(routes).to.have.property('postbet');
+    });
+
+    it('should have the set-result route', () => {
+      expect(routes).to.have.property('postsetresult');
+    });
+
+    it('should have the oracle route', () => {
+      expect(routes).to.have.property('postoracle');
+    });
+
+    it('should have the bet-start-block route', () => {
+      expect(routes).to.have.property('postbetstartblock');
+    });
+
+    it('should have the bet-end-block route', () => {
+      expect(routes).to.have.property('postbetendblock');
+    });
+
+    it('should have the result-set-start-block route', () => {
+      expect(routes).to.have.property('postresultsetstartblock');
+    });
+
+    it('should have the result-set-end-block route', () => {
+      expect(routes).to.have.property('postresultsetendblock');
+    });
+
+    it('should have the vote route', () => {
+      expect(routes).to.have.property('postvote');
+    });
+
+    it('should have the finalize-result route', () => {
+      expect(routes).to.have.property('postfinalizeresult');
+    });
+
+    it('should have the arbitration-end-block route', () => {
+      expect(routes).to.have.property('postarbitrationendblock');
+    });
+
+    it('should have the last-result-index route', () => {
+      expect(routes).to.have.property('postlastresultindex');
+    });
   });
 
-  it('post: get-account-address', () => {
-    expect(routes).to.have.property('postgetaccountaddress');
-  });
+  describe('GET', () => {
+    it('should have the get-wallet-info route', () => {
+      expect(routes).to.have.property('getgetwalletinfo');
+    });
 
-  it('post: get-transaction', () => {
-    expect(routes).to.have.property('postgettransaction');
-  });
+    it('should have the list-address-groupings route', () => {
+      expect(routes).to.have.property('getlistaddressgroupings');
+    });
 
-  it('get: get-wallet-info', () => {
-    expect(routes).to.have.property('getgetwalletinfo');
-  });
+    it('should have the list-unspent route', () => {
+      expect(routes).to.have.property('getlistunspent');
+    });
 
-  it('get: list-address-groupings', () => {
-    expect(routes).to.have.property('getlistaddressgroupings');
-  });
+    it('should have the get-blockchain-info route', () => {
+      expect(routes).to.have.property('getgetblockchaininfo');
+    });
 
-  it('get: list-unspent', () => {
-    expect(routes).to.have.property('getlistunspent');
-  });
-
-  it('post: wallet-passphrase', () => {
-    expect(routes).to.have.property('postwalletpassphrase');
-  });
-
-  it('post: wallet-lock', () => {
-    expect(routes).to.have.property('postwalletlock');
-  });
-
-  it('post: encrypt-wallet', () => {
-    expect(routes).to.have.property('postencryptwallet');
-  });
-
-  it('post: wallet-passphrase-change', () => {
-    expect(routes).to.have.property('postwalletpassphrasechange');
-  });
-
-  it('post: backup-wallet', () => {
-    expect(routes).to.have.property('postbackupwallet');
-  });
-
-  it('post: import-wallet', () => {
-    expect(routes).to.have.property('postimportwallet');
-  });
-
-  it('post: get-block', () => {
-    expect(routes).to.have.property('postgetblock');
-  });
-
-  it('get: get-blockchain-info', () => {
-    expect(routes).to.have.property('getgetblockchaininfo');
-  });
-
-  it('get: get-block-count', () => {
-    expect(routes).to.have.property('getgetblockcount');
-  });
-
-  it('post: get-block-hash', () => {
-    expect(routes).to.have.property('postgetblockhash');
-  });
-
-  it('post: get-transaction-receipt', () => {
-    expect(routes).to.have.property('postgettransactionreceipt');
-  });
-
-  it('post: search-logs', () => {
-    expect(routes).to.have.property('postsearchlogs');
-  });
-
-  it('post: event-escrow-amount', () => {
-    expect(routes).to.have.property('posteventescrowamount');
-  });
-
-  it('post: last-event-factory-index', () => {
-    expect(routes).to.have.property('postlasteventfactoryindex');
-  });
-
-  it('post: last-oracle-factory-index', () => {
-    expect(routes).to.have.property('postlastoraclefactoryindex');
-  });
-
-  it('post: approve', () => {
-    expect(routes).to.have.property('postapprove');
-  });
-
-  it('post: allowance', () => {
-    expect(routes).to.have.property('postallowance');
-  });
-
-  it('post: bot-balance', () => {
-    expect(routes).to.have.property('postbotbalance');
-  });
-
-  it('post: version', () => {
-    expect(routes).to.have.property('postversion');
-  });
-
-  it('post: get-result', () => {
-    expect(routes).to.have.property('postgetresult');
-  });
-
-  it('post: bet-balances', () => {
-    expect(routes).to.have.property('postbetbalances');
-  });
-
-  it('post: vote-balances', () => {
-    expect(routes).to.have.property('postvotebalances');
-  });
-
-  it('post: total-bets', () => {
-    expect(routes).to.have.property('posttotalbets');
-  });
-
-  it('post: total-votes', () => {
-    expect(routes).to.have.property('posttotalvotes');
-  });
-
-  it('post: create-topic', () => {
-    expect(routes).to.have.property('postcreatetopic');
-  });
-
-  it('post: event-factory-version', () => {
-    expect(routes).to.have.property('posteventfactoryversion');
-  });
-
-  it('post: withdraw', () => {
-    expect(routes).to.have.property('postwithdraw');
-  });
-
-  it('post: withdraw-escrow', () => {
-    expect(routes).to.have.property('postwithdrawescrow');
-  });
-
-  it('post: total-qtum-value', () => {
-    expect(routes).to.have.property('posttotalqtumvalue');
-  });
-
-  it('post: totalbotvalue', () => {
-    expect(routes).to.have.property('posttotalbotvalue');
-  });
-
-  it('post: final-result', () => {
-    expect(routes).to.have.property('postfinalresult');
-  });
-
-  it('post: status', () => {
-    expect(routes).to.have.property('poststatus');
-  });
-
-  it('post: did-withdraw', () => {
-    expect(routes).to.have.property('postdidwithdraw');
-  });
-
-  it('post: winnings', () => {
-    expect(routes).to.have.property('postwinnings');
-  });
-
-  it('post: event-address', () => {
-    expect(routes).to.have.property('posteventaddress');
-  });
-
-  it('post: consensus-threshold', () => {
-    expect(routes).to.have.property('postconsensusthreshold');
-  });
-
-  it('post: finished', () => {
-    expect(routes).to.have.property('postfinished');
-  });
-
-  it('post: bet', () => {
-    expect(routes).to.have.property('postbet');
-  });
-
-  it('post: set-result', () => {
-    expect(routes).to.have.property('postsetresult');
-  });
-
-  it('post: oracle', () => {
-    expect(routes).to.have.property('postoracle');
-  });
-
-  it('post: bet-start-block', () => {
-    expect(routes).to.have.property('postbetstartblock');
-  });
-
-  it('post: bet-end-block', () => {
-    expect(routes).to.have.property('postbetendblock');
-  });
-
-  it('post: result-set-start-block', () => {
-    expect(routes).to.have.property('postresultsetstartblock');
-  });
-
-  it('post: result-set-end-block', () => {
-    expect(routes).to.have.property('postresultsetendblock');
-  });
-
-  it('post: vote', () => {
-    expect(routes).to.have.property('postvote');
-  });
-
-  it('post: finalize-result', () => {
-    expect(routes).to.have.property('postfinalizeresult');
-  });
-
-  it('post: arbitration-end-block', () => {
-    expect(routes).to.have.property('postarbitrationendblock');
-  });
-
-  it('post: last-result-index', () => {
-    expect(routes).to.have.property('postlastresultindex');
+    it('should have the get-block-count route', () => {
+      expect(routes).to.have.property('getgetblockcount');
+    });
   });
 });
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,42 +2,44 @@ require('dotenv').config();
 const fs = require('fs-extra');
 const moment = require('moment');
 const winston = require('winston');
-
+const _ = require('lodash');
 const Utils = require('./utils');
 const { Config } = require('../config/config');
 
-const LOG_DIR = Utils.getLogDir();
+let logger;
+if (!_.includes(process.argv, '--test')) {
+  const logDir = Utils.getLogDir();
 
-// Create log dir if needed
-console.log(`Logs output: ${LOG_DIR}`);
-fs.ensureDirSync(LOG_DIR);
+  // Create log dir if needed
+  console.log(`Logs output: ${logDir}`);
+  fs.ensureDirSync(logDir);
 
-const winstonCfg = winston.config;
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      timestamp() {
-        return moment().format('YYYY-MM-DD HH:mm:ss');
-      },
-      formatter(options) {
-        return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
-      },
-    }),
-    new (winston.transports.File)({
-      filename: `${LOG_DIR}/bodhiapp_${moment().format('YYYYMMDD_HHmmss')}.log`,
-      timestamp() {
-        return moment().format('YYYY-MM-DD HH:mm:ss');
-      },
-      formatter(options) {
-        return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
-      },
-      json: false,
-    }),
-  ],
-  exitOnError: false,
-});
+  const winstonCfg = winston.config;
+  logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        timestamp() {
+          return moment().format('YYYY-MM-DD HH:mm:ss');
+        },
+        formatter(options) {
+          return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
+        },
+      }),
+      new (winston.transports.File)({
+        filename: `${logDir}/bodhiapp_${moment().format('YYYYMMDD_HHmmss')}.log`,
+        timestamp() {
+          return moment().format('YYYY-MM-DD HH:mm:ss');
+        },
+        formatter(options) {
+          return `${options.timestamp()} ${winstonCfg.colorize(options.level, options.level.toUpperCase())} ${(options.message ? options.message : '')} ${(options.meta && Object.keys(options.meta).length ? `\n\t${JSON.stringify(options.meta)}` : '')}`;
+        },
+        json: false,
+      }),
+    ],
+    exitOnError: false,
+  });
 
-const loglvl = process.env.loglvl || Config.DEFAULT_LOGLVL;
-logger.level = loglvl;
-
+  const loglvl = process.env.loglvl || Config.DEFAULT_LOGLVL;
+  logger.level = loglvl;
+}
 module.exports = logger;
