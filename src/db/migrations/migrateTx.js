3595654ddcbaa9fd6ba2d289306d0736ef752f8a
frankobe <mua08p@gmail.com>
2018-05-04 09:51:07 -0700

    rename migrated datafile instead of delete

--- src/db/migrations/migrateTx.js
+++ src/db/migrations/migrateTx.js
@@ -7,13 +7,14 @@ const Utils = require('../../utils/utils');
 const logger = require('../../utils/logger');
 
 async function appendDB(origin, dest) {
-  logger.info(`To append ${origin} to ${dest}`);
   if (fs.existsSync(origin)) {
+    logger.info(`To append ${origin} to ${dest}`);
     try {
       const fileContent = fs.readFileSync(origin);
       fs.appendFileSync(dest, fileContent);
       logger.info(`Success append ${origin} to ${dest}`);
-      await fs.unlink(origin);
+      await fs.rename(origin, `${origin}.old`);
+      logger.info(`Rename ${origin} to ${origin}.old`);
     } catch (err) {
       logger.error(`DB migration error: ${err.msg}`);
       throw err;
@@ -22,12 +23,12 @@ async function appendDB(origin, dest) {
 }
 
 async function migrateTxDB() {
-  const d0OriginFile = `${Utils.getBaseDataDir()}/c0_d0/nedb/transactions.db`;
-  const d1OriginFile = `${Utils.getBaseDataDir()}/c0_d1/nedb/transactions.db`;
-  const destFile = `${Utils.getLocalCacheDataDir()}/nedb/transactions.db`;
+  const d0OriginPath = `${Utils.getBaseDataDir()}/c0_d0/nedb/transactions.db`;
+  const d1OriginPath = `${Utils.getBaseDataDir()}/c0_d1/nedb/transactions.db`;
+  const destPath = `${Utils.getLocalCacheDataDir()}/nedb/transactions.db`;
 
-  await appendDB(d0OriginFile, destFile);
-  await appendDB(d1OriginFile, destFile);
+  await appendDB(d0OriginPath, destPath);
+  await appendDB(d1OriginPath, destPath);
 }
 
 module.exports = migrateTxDB;
