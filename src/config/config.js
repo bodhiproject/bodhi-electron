f65291fd33df99c489389d6cf0884c723c1a9e33
Deric Walintukan <deric.walintukan@gmail.com>
2018-05-04 16:25:36 -0700

    Remove config deployment flag

--- src/config/config.js
+++ src/config/config.js
@@ -21,9 +21,6 @@ const Config = {
   DEFAULT_GAS_PRICE: 0.0000004,
   CREATE_DORACLE_GAS_LIMIT: 1500000,
   UNLOCK_SECONDS: 86400,
-  deployment: {
-    TESTNET_ONLY: false,
-  },
 };
 
 let qtumEnv; // Qtumd environment var: testnet/mainnet
