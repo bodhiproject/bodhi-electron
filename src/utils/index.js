b344a0db01c5eb59bdd2623153c3457f1f5589d9
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-20 22:51:29 +0700

    Remove commented code

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,14 +5,9 @@ const { isDevEnv, getDevQtumExecPath } = require('../../server/src/utils');
 
 /*
 * Gets the prod env qtum exec path.
-* @param execFile {String} The exec file type needed to be returned.
 * return {String} The full prod path for the exec file.
 */
 const getProdQtumExecPath = () => {
-  // if (exec !== execFile.QTUMD && exec !== execFile.QTUM_QT && exec !== execFile.QTUM_CLI) {
-  //   throw Error(`Invalid execFile type: ${exec}`);
-  // }
-
   const platform = process.platform;
   const arch = process.arch;
   let osFolder;
@@ -43,13 +38,6 @@ const getProdQtumExecPath = () => {
 
   const { app } = require('electron');
   const path = `${app.getAppPath()}/server/qtum/${osFolder}/bin`;
-  // let path;
-  // if (platform === 'win32') {
-  //   path = `${app.getAppPath()}/server/qtum/${osFolder}/bin/${exec}.exe`;
-  // } else {
-  //   path = `${app.getAppPath()}/server/qtum/${osFolder}/bin/${exec}`;
-  // }
-
   return path.replace('app.asar', 'app.asar.unpacked');
 };
 
